c1975902475d1e461ad2758bc228d8062e37b689
Arko <arkoroyb@gmail.com>
2023-03-19 21:02:57 +0600

    some responsiveness issue fixed

--- pages/hc/HcBody.tsx
+++ pages/hc/HcBody.tsx
@@ -20,7 +20,7 @@ const HcBody = () => {
                 </div>
             </section>
             <section>
-                <div className="bg-[#f5f6f7] mt-16">
+                <div className="bg-[#f5f6f7] mt-16 mx-5 md:mx-0">
                     <div className="">
                         <p className="text-[#2A5BD7] py-4 text-center border cursor-pointer ">
                             <Link href="/" className='hover:border-b hover:border-blue-400'>How to check a links destination</Link>
@@ -30,7 +30,7 @@ const HcBody = () => {
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
                     {
                         HcCards.map(card =>
-                            <div onClick={() => router.push(`hc/${card.slug}`)} key={card.id} className="text-[#2a5bd7] border border-[#2a5bd7] py-8 px-10 text-center cursor-pointer hover:bg-[#2a5bd7] hover:text-[#fff] transition-all ease-in">
+                            <div onClick={() => router.push(`hc/${card.slug}`)} key={card.id} className="text-[#2a5bd7] border border-[#2a5bd7] py-8 px-10 text-center cursor-pointer hover:bg-[#2a5bd7] hover:text-[#fff] transition-all ease-in mx-5 md:mx-0">
                                 <h4 className="text-[18px] font-bold">{card.title}</h4>
                                 <p className="mt-3">{card.detail}</p>
                             </div>
@@ -38,7 +38,7 @@ const HcBody = () => {
                     }
                 </div>
             </section>
-            <section className='py-20'>
+            <section className='py-20 mx-5 md:mx-0'>
                 <h3 className="text-[18px] font-semibold">Promoted articles</h3>
                 <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
                     {
@@ -50,7 +50,7 @@ const HcBody = () => {
                             </div>)
                     }
                 </div>
-                <div className="bg-[#f5f6f7] mt-16 mx-32 text-[15px]">
+                <div className="bg-[#f5f6f7] mt-16 mx-5 md:mx-32 text-[15px]">
                     <p className=" py-4 text-center border cursor-pointer">Get more from your links.
                         <Link href="/" className='text-[#2A5BD7] hover:border-b hover:border-blue-400'> Check out our plans and features.</Link>
                     </p>
